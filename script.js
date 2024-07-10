a6584b2dc3b2c6b4f37c839ffa5acdd7b38b0f23
Hazem yakout <143601439+hazem-yakout@users.noreply.github.com>
2024-07-11 00:21:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,65 @@
 
-const apiKey = "4b16dceec3745598edf7c95d784a4096";
-const generateBtn = document.getElementById("generateBtn");
-const promptInput = document.getElementById("prompt");
+
+const api = "4b16dceec3745598edf7c95d784a4096";
+const inp = document.getElementById("inp");
 const images = document.querySelector(".images");
 
-async function generateImage(prompt) {
-  const response = await fetch("https://api.openai.com/v1/images/generations", {
+// make a request to open api
+async function getImage() {
+  const methods = {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${apiKey}`
-    },
+
     body: JSON.stringify({
-      prompt: prompt,
-      n: 1,
-      size: "1024x1024"
-    })
-  });
+      data: {
+        id: "2ndCYJK",
+        title: "c1f64245afb2",
+        url_viewer: "https://ibb.co/2ndCYJK",
+        url: "https://i.ibb.co/w04Prt6/c1f64245afb2.gif",
+        display_url: "https://i.ibb.co/98W13PY/c1f64245afb2.gif",
+        width: "1",
+        height: "1",
+        size: "42",
+        time: "1552042565",
+        expiration: "0",
+        image: {
+          filename: "c1f64245afb2.gif",
+          name: "c1f64245afb2",
+          mime: "image/gif",
+          extension: "gif",
+          url: "https://i.ibb.co/w04Prt6/c1f64245afb2.gif",
+        },
+        thumb: {
+          filename: "c1f64245afb2.gif",
+          name: "c1f64245afb2",
+          mime: "image/gif",
+          extension: "gif",
+          url: "https://i.ibb.co/2ndCYJK/c1f64245afb2.gif",
+        },
+        medium: {
+          filename: "c1f64245afb2.gif",
+          name: "c1f64245afb2",
+          mime: "image/gif",
+          extension: "gif",
+          url: "https://i.ibb.co/98W13PY/c1f64245afb2.gif",
+        },
+        delete_url: "https://ibb.co/2ndCYJK/670a7e48ddcb85ac340c717a41047e5c",
+      },
+      success: true,
+      status: 200,
+    }),
+  };
+  const res = await fetch("https://api.imgbb.com/1/upload", methods);
+  //parse the response as json
+  const data = await res.json();
 
-  const data = await response.json();
-  if (data.data && data.data.length > 0) {
-    const imageUrl = data.data[0].url;
-    const img = document.createElement("img");
-    img.src = imageUrl;
-    images.appendChild(img);
-  } else {
-    console.error("No image generated.");
-  }
+  const listimages = data.data;
+  const x =
+    listimages &&
+    listimages.map((photo) => {
+      const cont = document.createElement("div");
+      images.append(cont);
+      const img = document.createElement("img");
+      cont.append(img);
+      img.src = photo.url;
+    });
 }
-
-generateBtn.addEventListener("click", () => {
-  const prompt = promptInput.value;
-  if (prompt) {
-    generateImage(prompt);
-  } else {
-    alert("Please enter a prompt.");
-  }
-});
