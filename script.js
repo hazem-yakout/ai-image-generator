1cd0de999ea9506e6b63447a2f05f16f563b35ab
Hazem yakout <143601439+hazem-yakout@users.noreply.github.com>
2024-07-11 00:06:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,31 @@
+
 const api = "4b16dceec3745598edf7c95d784a4096";
 const inp = document.getElementById("inp");
 const images = document.querySelector(".images");
 
-// make a request to open api
 async function getImage() {
+  const formData = new FormData();
+  formData.append('image', inp.files[0]);
+
   const methods = {
     method: "POST",
-
-    body: JSON.stringify({
-      data: {
-        id: "2ndCYJK",
-        title: "c1f64245afb2",
-        url_viewer: "https://ibb.co/2ndCYJK",
-        url: "https://i.ibb.co/w04Prt6/c1f64245afb2.gif",
-        display_url: "https://i.ibb.co/98W13PY/c1f64245afb2.gif",
-        width: "1",
-        height: "1",
-        size: "42",
-        time: "1552042565",
-        expiration: "0",
-        image: {
-          filename: "c1f64245afb2.gif",
-          name: "c1f64245afb2",
-          mime: "image/gif",
-          extension: "gif",
-          url: "https://i.ibb.co/w04Prt6/c1f64245afb2.gif",
-        },
-        thumb: {
-          filename: "c1f64245afb2.gif",
-          name: "c1f64245afb2",
-          mime: "image/gif",
-          extension: "gif",
-          url: "https://i.ibb.co/2ndCYJK/c1f64245afb2.gif",
-        },
-        medium: {
-          filename: "c1f64245afb2.gif",
-          name: "c1f64245afb2",
-          mime: "image/gif",
-          extension: "gif",
-          url: "https://i.ibb.co/98W13PY/c1f64245afb2.gif",
-        },
-        delete_url: "https://ibb.co/2ndCYJK/670a7e48ddcb85ac340c717a41047e5c",
-      },
-      success: true,
-      status: 200,
-    }),
+    body: formData,
   };
-  const res = await fetch("https://api.imgbb.com/1/upload", methods);
-  //parse the response as json
+
+  const res = await fetch(`https://api.imgbb.com/1/upload?key=${api}`, methods);
   const data = await res.json();
 
-  const listimages = data.data;
-  const x =
-    listimages &&
-    listimages.map((photo) => {
-      const cont = document.createElement("div");
-      images.append(cont);
-      const img = document.createElement("img");
-      cont.append(img);
-      img.src = photo.url;
-    });
+  if (data.success) {
+    const photo = data.data;
+    const cont = document.createElement("div");
+    images.append(cont);
+    const img = document.createElement("img");
+    cont.append(img);
+    img.src = photo.url;
+  } else {
+    console.error('Error uploading image:', data);
+  }
 }
+
+// Add an event listener to call getImage when a file is selected
+inp.addEventListener('change', getImage);
